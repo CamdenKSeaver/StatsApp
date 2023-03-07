3cd38648a82595a26ba87de4792b4d89947af0d5
ZGaffoor <90778249+ZGaffoor@users.noreply.github.com>
2023-03-07 08:20:15 -0600

    create team updates

--- screens/CreateTeam.js
+++ screens/CreateTeam.js
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { StackActions } from '@react-navigation/native';
 import MyButton from '../App/components/MyButton';
+import {useImage} from 'react-image'
 
 export default function CreateTeam({ navigation, route }) {
   return (
     <View style={styles.container}>
-      <MyButton 
-        title='Save Team'
-        onPress={() => navigation.push("Home")}
-      />
-      <View style={styles.space} />
-      <MyButton 
-        title='Back'
-        onPress={() => {
-          navigation.navigate("Home")}}
-      />
+      <Image
+      style= {{width: 330, height:55}}
+          source={require('../App/assets/images/logo.png')} 
+          />
+      <View style={styles.buttonContainer}>
+        <MyButton 
+          title='Save Team'
+          onPress={() => navigation.push("Home")}
+        />
+        <View style={styles.space} />
+        <MyButton 
+          title='Back'
+          onPress={() => {
+            navigation.navigate("Home")}}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -23,18 +30,23 @@ export default function CreateTeam({ navigation, route }) {
 
 const styles = StyleSheet.create({
   container: {
-    headerBackTitleVisible: false,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center', 
+  },
+
+  buttonContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'center', 
-    paddingBottom: '125%',
+    paddingBottom: '100%',
   },
 
   space: {
     width: 20, 
-    height: 20,
   },
 
 });
